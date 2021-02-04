7f52e65f532822c289a86986f93cb38c04053565
Dmytro <ivanskoy.d@gmail.com>
2021-02-04 18:43:55 +0200

    Add controller

---
+++ src/PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlayBall.GroupManagement.Web.Controllers
+{
+    [Route("groups")]
+    public class GroupsController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Content("Helloooo!");
+        }
+    }
+}
--- src/PlayBall.GroupManagement.Web/Startup.cs
+++ src/PlayBall.GroupManagement.Web/Startup.cs
@@ -16,6 +16,8 @@ namespace PlayBall.GroupManagement.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +29,9 @@ namespace PlayBall.GroupManagement.Web
             }
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
